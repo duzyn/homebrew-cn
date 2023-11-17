af45cb6fa3dda33d3beb370d1bff486c6db4ceb1
David Peng <duzyn@users.noreply.github.com>
2023-11-17 23:23:12 +0800

    use mirror.ghproxy.com

--- update.sh
+++ update.sh
@@ -20,9 +20,9 @@ cd "$SCRIPT_DIR" || exit 1
 
 : "${TEST:="false"}"
 if [[ "$TEST" == "true" ]]; then
-    git clone --depth 1 https://ghproxy.com/https://github.com/Homebrew/homebrew-core
-    git clone --depth 1 https://ghproxy.com/https://github.com/Homebrew/homebrew-cask
-    git clone --depth 1 https://ghproxy.com/https://github.com/Homebrew/homebrew-cask-versions
+    git clone --depth 1 https://mirror.ghproxy.com/https://github.com/Homebrew/homebrew-core
+    git clone --depth 1 https://mirror.ghproxy.com/https://github.com/Homebrew/homebrew-cask
+    git clone --depth 1 https://mirror.ghproxy.com/https://github.com/Homebrew/homebrew-cask-versions
 fi
 
 rm -rf ./Formula ./Casks ./Aliases
@@ -34,10 +34,10 @@ cp -r ./homebrew-cask-versions/Casks/* ./Casks/
 rm -rf ./homebrew-core ./homebrew-cask ./homebrew-cask-versions
 
 for file in ./Formula/**/*.rb ./Casks/**/*.rb; do
-    perl -pi -e 's#(github\.com/.+/releases/download)#ghproxy.com/https://\1#g;
-                 s#(raw\.githubusercontent\.com)#ghproxy.com/https://\1#g;
-                 s#(github\.com/.+/raw)#ghproxy.com/https://\1#g;
-                 s#(github\.com/.+/archive)#ghproxy.com/https://\1#g;
+    perl -pi -e 's#(github\.com/.+/releases/download)#mirror.ghproxy.com/https://\1#g;
+                 s#(raw\.githubusercontent\.com)#mirror.ghproxy.com/https://\1#g;
+                 s#(github\.com/.+/raw)#mirror.ghproxy.com/https://\1#g;
+                 s#(github\.com/.+/archive)#mirror.ghproxy.com/https://\1#g;
                  s#(downloads\.sourceforge\.net/.*)"#\1?use_mirror=nchc"#g;
                  s#download\.kde\.org#mirrors.ustc.edu.cn/kde#g;
                  s#(www\.)?7-zip\.org/a#mirror.nju.edu.cn/7-zip#g;
@@ -47,11 +47,11 @@ for file in ./Formula/**/*.rb ./Casks/**/*.rb; do
                  s#go\.dev/dl#mirrors.aliyun.com/golang#g;
                  s#download\.videolan\.org#mirrors.aliyun.com/videolan#g;
                  s#media\.inkscape\.org/dl/resources/file#mirror.nju.edu.cn/inkscape#g;
-                 s#dbeaver\.io/files#ghproxy.com/https://github.com/dbeaver/dbeaver/releases/download#g;
+                 s#dbeaver\.io/files#mirror.ghproxy.com/https://github.com/dbeaver/dbeaver/releases/download#g;
                  s#cdn-fastly\.obsproject\.com/downloads/obs-studio-(.+)-macos#mirrors.nju.edu.cn/github-release/obsproject/obs-studio/OBS%20Studio%20\1/obs-studio-\1-macos#g;
                  s#download\.gimp\.org/gimp#mirrors.aliyun.com/gimp/gimp#g;
                  s#download\.blender\.org#mirrors.aliyun.com/blender#g;
                  s#download\.virtualbox\.org/virtualbox#mirror.nju.edu.cn/virtualbox/#g;
                  s#www\.wireshark\.org/download#mirror.nju.edu.cn/wireshark/#g;
-                 s#download\.calibre-ebook\.com/#ghproxy.com/https://github.com/kovidgoyal/calibre/releases/download/v#g' "$file"
+                 s#download\.calibre-ebook\.com/#mirror.ghproxy.com/https://github.com/kovidgoyal/calibre/releases/download/v#g' "$file"
 done
